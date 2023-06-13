b8788231d507892bfd549b893d19e44c818c6f6c
Dhaffa Agus <73971391+not-xygen@users.noreply.github.com>
2023-06-13 23:14:52 +0800

    feat: finalizing

--- app.py
+++ app.py
@@ -153,7 +153,3 @@ def index():
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
